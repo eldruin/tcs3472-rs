15df20bff690906ba6f6f33b23492a62a9ebe523
Diego Barrios Romero <eldruin@gmail.com>
2018-10-21 16:32:46 +0200

    Refactor tests

--- tests/configuration.rs
+++ tests/configuration.rs
@@ -5,39 +5,29 @@ mod common;
 use common::{ setup, check_sent_data, Register, BitFlags };
 use tcs3472::{ Error, RgbCGain };
 
-const ENABLE_CMD : u8 = BitFlags::CMD | Register::ENABLE;
-
-#[test]
-fn can_enable() {
-    let mut dev = setup(&[0]);
-    dev.enable().unwrap();
-    check_sent_data(dev, &[ENABLE_CMD, BitFlags::POWER_ON]);
+macro_rules! enable_disable_test {
+    ($name:ident, $first_method:ident, $second_method:ident, $register:ident, $expected:expr) => {
+        #[test]
+        fn $name() {
+            let mut dev = setup(&[0]);
+            dev.$first_method().unwrap();
+            dev.$second_method().unwrap();
+            check_sent_data(dev, &[BitFlags::CMD | Register::$register, $expected]);
+        }
+    };
 }
+enable_disable_test!(can_enable, disable,  enable, ENABLE, BitFlags::POWER_ON);
+enable_disable_test!(can_disable, enable, disable, ENABLE,                  0);
 
-#[test]
-fn can_disable() {
-    let mut dev = setup(&[0]);
-    dev.enable().unwrap();
-    dev.disable().unwrap();
-    check_sent_data(dev, &[ENABLE_CMD, 0]);
-}
+enable_disable_test!(can_enable_rgbc, disable_rgbc,  enable_rgbc, ENABLE, BitFlags::RGBC_EN);
+enable_disable_test!(can_disable_rgbc, enable_rgbc, disable_rgbc, ENABLE,                 0);
 
-#[test]
-fn can_enable_rgbc() {
-    let mut dev = setup(&[0]);
-    dev.enable().unwrap();
-    dev.enable_rgbc().unwrap();
-    check_sent_data(dev, &[ENABLE_CMD, BitFlags::POWER_ON | BitFlags::RGBC_EN]);
-}
+enable_disable_test!(can_enable_wait, disable_wait,  enable_wait, ENABLE, BitFlags::WAIT_EN);
+enable_disable_test!(can_disable_wait, enable_wait, disable_wait, ENABLE,                 0);
 
-#[test]
-fn can_disable_rgbc() {
-    let mut dev = setup(&[0]);
-    dev.enable().unwrap();
-    dev.enable_rgbc().unwrap();
-    dev.disable_rgbc().unwrap();
-    check_sent_data(dev, &[ENABLE_CMD, BitFlags::POWER_ON & !BitFlags::RGBC_EN]);
-}
+enable_disable_test!(can_enable_wait_long, disable_wait_long,  enable_wait_long, CONFIG, BitFlags::WLONG);
+enable_disable_test!(can_disable_wait_long, enable_wait_long, disable_wait_long, CONFIG,               0);
+
 
 macro_rules! set_rgbc_gain_test {
     ($name:ident, $variant:ident, $expected:expr) => {
@@ -89,32 +79,3 @@ set_param_test!(can_set_ic_256, set_integration_cycles, 256, ATIME, 0x00);
 set_param_test!(can_set_wc_1,   set_wait_cycles,   1, WTIME, 0xFF);
 set_param_test!(can_set_wc_85,  set_wait_cycles,  85, WTIME, 0xAB);
 set_param_test!(can_set_wc_256, set_wait_cycles, 256, WTIME, 0x00);
-
-#[test]
-fn can_enable_wait_long() {
-    let mut dev = setup(&[0]);
-    dev.enable_wait_long().unwrap();
-    check_sent_data(dev, &[BitFlags::CMD | Register::CONFIG, BitFlags::WLONG]);
-}
-
-#[test]
-fn can_disable_wait_long() {
-    let mut dev = setup(&[0]);
-    dev.disable_wait_long().unwrap();
-    check_sent_data(dev, &[BitFlags::CMD | Register::CONFIG, 0]);
-}
-
-#[test]
-fn can_enable_wait() {
-    let mut dev = setup(&[0]);
-    dev.enable_wait().unwrap();
-    check_sent_data(dev, &[ENABLE_CMD, BitFlags::WAIT_EN]);
-}
-
-#[test]
-fn can_disable_wait() {
-    let mut dev = setup(&[0]);
-    dev.enable_wait().unwrap();
-    dev.disable_wait().unwrap();
-    check_sent_data(dev, &[ENABLE_CMD, 0]);
-}
