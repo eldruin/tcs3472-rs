490d021fefc4831a9be392cf0eeeb2d811aa343a
Szczepan Cieślik <szczepan.cieslik@gmail.com>
2024-12-28 14:49:19 +0100

    Move embassy example

--- src/lib.rs
+++ src/lib.rs
@@ -151,31 +151,7 @@
 //!
 //! ### Using async driver
 //!
-//! Enable the `async` feature in your `Cargo.toml`:
-//! ```toml
-//! tcs3472 = { version = "0.3.0", features = ["async"] }
-//! ```
-//!
-//! Using async driver with [Embassy](https://embassy.dev/) framework:
-//! ```no_run
-//! #[embassy_executor::main]
-//! async fn main(_spawner: Spawner) {
-//!     let p = embassy_stm32::init(Default::default());
-//!     let mut i2c = I2c::new(..);
-//!     let mut sensor = Tcs3472::new(i2c);
-//!     sensor.enable().await.unwrap();
-//!     sensor.enable_rgbc().await.unwrap();
-//!     while !sensor.is_rgbc_status_valid().await.unwrap() {
-//!         // wait for measurement to be available
-//!     };
-//!
-//!     let measurement = sensor.read_all_channels().await.unwrap();
-//!
-//!     defmt::info!("Measurements: clear = {}, red = {}, green = {}, blue = {}",
-//!          measurement.clear, measurement.red, measurement.green,
-//!          measurement.blue);
-//! }
-//! ```
+//! See examples/embassy.rs to see code for async case.
 
 #![deny(unsafe_code, missing_docs)]
 #![no_std]
